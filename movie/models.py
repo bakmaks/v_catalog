453b0c95e87298183036057ece0de84805bdcba8
bakmaks <bakmaks@gmail.com>
2021-02-09 17:49:25 +0300

    Добавлена модель Category.

--- movie/models.py
+++ movie/models.py
@@ -9,20 +9,21 @@ from .utils import code_str
 # Абстрактная модель для создания моделей Фильмов и сериалов
 class AbstractVideo(models.Model):
     ru_title = models.CharField(max_length=200, verbose_name='Название', blank=False)
-    en_title = models.CharField(max_length=200, verbose_name='Оригинальное Название')
+    en_title = models.CharField(max_length=200, verbose_name='Оригинальное Название', blank=True)
     product_year = models.PositiveSmallIntegerField(
-        verbose_name='Год', default=2020, validators=[validators.MinValueValidator(1900),
+        verbose_name='Год', default=2020, blank=True, validators=[validators.MinValueValidator(1900),
                                                       validators.MaxValueValidator(2100)])
-    slug = models.SlugField(max_length=100, unique=True)
-    country = models.CharField(verbose_name='Старна', max_length=50)
+    slug = models.SlugField(max_length=100, blank=True, unique=True)
+    country = models.CharField(verbose_name='Старна', blank=True, max_length=50)
     description = models.TextField(verbose_name='Описание', max_length=1000)
-    poster = models.ImageField(upload_to='images/%Y/%m/%d/')
-    IMDB_rating = models.DecimalField(verbose_name='IMDB', max_digits=1, decimal_places=1,
+    poster = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True)
+    IMDB_rating = models.DecimalField(verbose_name='IMDB', blank=True, max_digits=1, decimal_places=1,
                                       validators=[validators.MaxValueValidator(9.9),
                                                   validators.MinValueValidator(0.0)])
-    KPoisk_rating = models.DecimalField(verbose_name='Кинопоиск', max_digits=1, decimal_places=1,
+    KPoisk_rating = models.DecimalField(verbose_name='Кинопоиск', blank=True, max_digits=1, decimal_places=1,
                                         validators=[validators.MaxValueValidator(9.9),
                                                     validators.MinValueValidator(0.0)])
+    category = models.ManyToManyField('Category', blank=True)
 
     class Meta:
         abstract = True
@@ -36,8 +37,22 @@ class AbstractVideo(models.Model):
         super().save(*args, **kwargs)
 
 
+class Category(models.Model):
+    title = models.CharField(verbose_name='Жанр', max_length=50)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        # Создаёт уникальный слаг
+        self.slug = slugify(code_str(self.title) + str(time.time()))
+        super().save(*args, **kwargs)
+
+
 # Модель  фильмов
 class Film(AbstractVideo):
+
     class Meta:
         ordering = ['ru_title']
 

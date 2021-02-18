94b5bca141fd0f9f2577a2315408d433d9f46fdf
bakmaks <bakmaks@gmail.com>
2021-02-18 17:07:14 +0300

    --

--- movie/views.py
+++ movie/views.py
@@ -13,11 +13,6 @@ def index(request):
     return render(request, 'movie/base_films_list.html', {'films': films})
 
 
-# class RedirectToMain(RedirectView):
-#     def get_redirect_url(self, *args, **kwargs):
-#         return super().get_redirect_url(*args, **kwargs)
-
-
 # def redirect_to_movie(request):
 #     logger.debug(request.GET)
 #     return redirect('movie/film/')
